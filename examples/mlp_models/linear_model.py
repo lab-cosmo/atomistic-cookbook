d8dd98a19cee9ab8f90b196276c2946f3ec8b48c
Philip Loche <ploche@physik.fu-berlin.de>
2023-07-10 14:41:56 +0200

    Add linting including gh action
    Co-authored-by: Alexander Goscinski <alex.goscinski@posteo.de>

--- examples/mlp_models/linear_model.py
+++ examples/mlp_models/linear_model.py
@@ -10,11 +10,11 @@ MLP with Linear Model
 
 
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 # %%
 # Explanation
 
-X = np.random.rand(10000)*400
-Y = 0.1*X+10.1
-Y_p = Y + np.random.randn(10000)*1.5
+X = np.random.rand(10000) * 400
+Y = 0.1 * X + 10.1
+Y_p = Y + np.random.randn(10000) * 1.5
