598dd30ed5bab93fed0c4becb8fc972b48027c21
Luthaf <luthaf@luthaf.fr>
2023-10-17 18:06:18 +0200

    Use separate conda env for each example

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,30 +1,8 @@
-# Sphinx documentation build configuration file
-import os
-
-
 # Add any Sphinx extension module names here, as strings.
 extensions = [
-    "sphinx.ext.autodoc",  # import the modules you are documenting
-    "sphinx.ext.intersphinx",  # generate links to external projects
-    "sphinx.ext.viewcode",  # add links to highlighted source code
-    "sphinx_gallery.gen_gallery",  # provides a source parser for *.ipynb files
+    "sphinx.ext.viewcode",
+    "sphinx_gallery.load_style",
 ]
-
-examples_root = os.path.join(os.getcwd(), "../../examples/")
-examples_subdirs = []
-for path in os.listdir(examples_root):
-    # ignores files and hidden directories
-    if os.path.isdir(os.path.join(examples_root, path)) and path[0] != ".":
-        examples_subdirs.append(path)
-
-sphinx_gallery_conf = {
-    "filename_pattern": "/*",
-    "examples_dirs": [f"../../examples/{subdir}" for subdir in examples_subdirs],
-    "gallery_dirs": [f"{subdir}" for subdir in examples_subdirs],
-    "min_reported_time": 60,
-    # Make the code snippet for own functions clickable
-    "reference_url": {"cosmo-software-cookbook": None},
-}
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build"]
@@ -34,47 +12,3 @@ copyright = "BSD 3-Clause License, Copyright (c) 2023, COSMO software cookbook t
 
 htmlhelp_basename = "COSMO software-cookbook"
 html_theme = "furo"
-
-# We create the index.rst here because sphinx is not able to automatically
-# include all subdirectories using regex expression
-root_index_rst_content = r"""
-COSMO Software Cookbook
-=======================
-
-.. include:: ../../README.rst
-   :start-after: marker-intro-start
-   :end-before: marker-intro-end
-
-.. toctree::
-   :caption: Table of Contents
-
-"""
-root_index_rst_content += "".join(
-    [f"   {subdir}/index\n" for subdir in examples_subdirs]
-)
-print("Creating index.rst including all examples")
-print(root_index_rst_content)
-
-with open("index.rst", "w") as f:
-    f.write(root_index_rst_content)
-
-# Configuration for intersphinx: refer to the Python standard library
-# and other packages used by the cookbook
-
-intersphinx_mapping = {
-    "ase": ("https://wiki.fysik.dtu.dk/ase/", None),
-    "chemiscope": ("https://chemiscope.org/docs/", None),
-    "metatensor": ("https://lab-cosmo.github.io/metatensor/latest/", None),
-    "equisolve": ("https://lab-cosmo.github.io/equisolve/latest/", None),
-    "matplotlib": ("https://matplotlib.org/stable/", None),
-    "numpy": ("https://numpy.org/doc/stable/", None),
-    "python": ("https://docs.python.org/3", None),
-    "rascaline": ("https://luthaf.fr/rascaline/latest/", None),
-    "rascal": ("https://lab-cosmo.github.io/librascal/", None),
-    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
-    "sklearn": (
-        "http://scikit-learn.org/stable",
-        (None, "./_intersphinx/sklearn-objects.inv"),
-    ),
-    "skmatter": ("https://scikit-matter.readthedocs.io/en/latest/", None),
-}
---
+++ generate-gallery.py
@@ -0,0 +1,68 @@
+import os
+import shutil
+import sys
+
+import sphinx_gallery.gen_gallery
+
+
+HERE = os.path.realpath(os.path.dirname(__file__))
+
+
+class AttrDict(dict):
+    def __init__(self):
+        super().__init__()
+        self.__dict__ = self
+
+
+class PseudoSphinxApp:
+    """
+    Class pretending to be a sphinx App, used to configure and run sphinx-gallery
+    from the command line, without having an actual sphinx project.
+    """
+
+    def __init__(self, example):
+        gallery_dir = os.path.join(
+            HERE, "docs", "src", "examples", os.path.basename(example)
+        )
+        if os.path.exists(gallery_dir):
+            shutil.rmtree(gallery_dir)
+
+        # the options here are the minimal set of options to get sphinx-gallery to run
+        # feel free to add more if sphinx-gallery uses more options in the future
+        self.config = AttrDict()
+        self.config.html_static_path = []
+        self.config.source_suffix = [".rst"]
+        self.config.default_role = ""
+        self.config.sphinx_gallery_conf = {
+            "filename_pattern": ".*",
+            "examples_dirs": os.path.join(HERE, example),
+            "gallery_dirs": gallery_dir,
+            "min_reported_time": 60,
+        }
+
+        self.builder = AttrDict()
+        self.builder.srcdir = os.path.join(HERE, "docs", "src")
+        self.builder.outdir = ""
+        self.builder.name = os.path.basename(example)
+
+        self.extensions = []
+
+        self.builder.config = AttrDict()
+        self.builder.config.plot_gallery = "True"
+        self.builder.config.abort_on_example_error = True
+        self.builder.config.highlight_language = None
+
+    def add_css_file(self, path):
+        pass
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(f"usage: {sys.argv[0]} <example/dir>")
+        sys.exit(1)
+
+    app = PseudoSphinxApp(example=sys.argv[1])
+
+    sphinx_gallery.gen_gallery.fill_gallery_conf_defaults(app, app.config)
+    sphinx_gallery.gen_gallery.update_gallery_conf_builder_inited(app)
+    sphinx_gallery.gen_gallery.generate_gallery_rst(app)
