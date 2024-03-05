46a236dcef32813a986591129b2ece1e9868d5a0
Philip Loche <ploche@physik.fu-berlin.de>
2024-03-05 12:45:50 +0100

    more debug

--- examples/cp2k_run_batch/return_CP2K_versions.sh
+++ examples/cp2k_run_batch/return_CP2K_versions.sh
@@ -1,4 +1,11 @@
 #! /bin/bash
 
+set -ex
+
+conda_path=$(conda info --envs | grep 'cp2k_run_batch' | sed 's: ::g')
+
+echo "all executables for cp2k_run_batch conda env"
+ls $conda_path/bin
+
 echo "List all exectubales containing 'cp2k'"
 compgen -cX '!*cp2k*'
