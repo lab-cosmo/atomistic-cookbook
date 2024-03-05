e66b8d2508ff4db8a4f87985b9d00b0fa2ead79b
Philip Loche <ploche@physik.fu-berlin.de>
2024-03-05 12:18:15 +0100

    more debug

--- examples/cp2k_run_batch/return_CP2K_versions.sh
+++ examples/cp2k_run_batch/return_CP2K_versions.sh
@@ -1,12 +1,4 @@
 #! /bin/bash
 
-echo "cp2k.ssmp"
-which cp2k.ssmp
-echo "cp2k.popt"
-which cp2k.popt
-echo "cp2k.psmp"
-which cp2k.psmp
-echo "cp2k.sopt"
-which cp2k.sopt
-echo "cp2k"
-which cp2k
+echo "List all exectubales containing 'cp2k'"
+compgen -cX '!*cp2k*'
