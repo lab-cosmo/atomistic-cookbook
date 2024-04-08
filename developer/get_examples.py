256b320c6de7f006502ac88129f9cdcad2a86446
Philip Loche <ploche@physik.fu-berlin.de>
2024-04-08 18:37:45 +0200

    Generate example matrix dynamically (#58)

---
+++ developer/get_examples.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Script to create JSON string suitable for a github action matrix."""
+import glob
+import json
+import os
+
+
+ROOT = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+EXAMPLES = os.path.join(ROOT, "examples")
+
+
+def get_examples():
+    """The current list of examples, determined from the directories on disk"""
+    return [
+        os.path.basename(os.path.normpath(file)) for file in glob.glob(f"{EXAMPLES}/*")
+    ]
+
+
+def create_json():
+    """A JSON string suitable for a github action matrix."""
+    return json.dumps({"example-name": get_examples()})
+
+
+if __name__ == "__main__":
+    print(create_json())
