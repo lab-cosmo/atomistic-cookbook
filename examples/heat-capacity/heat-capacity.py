c985c0c9705d324a75d64091cbe11cefd6991b92
frostedoyster <bigi.f@libero.it>
2024-09-18 10:25:34 +0200

    Better comments for the heat capacity recipe

--- examples/heat-capacity/heat-capacity.py
+++ examples/heat-capacity/heat-capacity.py
@@ -4,9 +4,10 @@ Path integral molecular dynamics
 
 :Authors: Michele Ceriotti `@ceriottm <https://github.com/ceriottm/>`_
 
-This example shows how to run a path integral molecular dynamics
-simulation using ``i-PI``, analyze the output and visualize the
-trajectory in ``chemiscope``. It uses `LAMMPS <http://lammps.org>`_
+This example shows how to estimate the heat capacity of liquid water
+from a path integral molecular dynamics simulation. The dynamics are
+run with `i-PI <http://ipi-code.org>`_, and 
+`LAMMPS <http://lammps.org>`_ is used
 as the driver to simulate the `q-TIP4P/f water
 model <http://doi.org/10.1063/1.3167790>`_.
 """
@@ -20,51 +21,26 @@ import numpy as np
 
 
 # %%
-# Quantum nuclear effects and path integral methods
-# -------------------------------------------------
+# Quantum heat capacity of water
+# ------------------------------
 #
-# The Born-Oppenheimer approximation separates the joint quantum mechanical
-# problem for electrons and nuclei into two independent problems. Even though
-# often one makes the additional approximation of treating nuclei as classical
-# particles, this is not necessary, and in some cases (typically when H atoms are
-# present) can add considerable error.
+# As introduced in the ``path-integrals`` example, path-integral estimators
+# for observables that depend on momenta are generally not trivial to compute.
 #
-#
-# .. figure:: pimd-slices-round.png
-#    :align: center
-#    :width: 600px
-#
-#    A representation of ther ring-polymer Hamiltonian for a water molecule.
-#
-# In order to describe the quantum mechanical nature of light nuclei
-# (nuclear quantum effects) one of the most widely-applicable methods uses
-# the *path integral formalism*  to map the quantum partition function of a
-# set of distinguishable particles onto the classical partition function of
-# *ring polymers* composed by multiple beads (replicas) with
-# corresponding atoms in adjacent replicas being connected by harmonic
-# springs.
-# `The textbook by Tuckerman <https://tinyurl.com/bdfhk2tx>`_
-# contains a pedagogic introduction to the topic, while
-# `this paper <https://doi.org/10.1063/1.3489925>`_ outlines the implementation
-# used in ``i-PI``.
-#
-# The classical partition function of the path converges to quantum statistics
-# in the limit of a large number of replicas. In this example, we will use a
-# technique based on generalized Langevin dynamics, known as
-# `PIGLET <http://doi.org/10.1103/PhysRevLett.109.100604>`_ to accelerate the
-# convergence.
+# In this example, we will focus on the heat capacity, which is one such
+# observable, and we will calculate it for liquid water at room temperature.
 
 
 # %%
-# Running PIMD calculations with ``i-PI``
-# ---------------------------------------
+# Running the PIMD calculation
+# ----------------------------
 #
-# `i-PI <http://ipi-code.org>`_ is based on a client-server model, with ``i-PI``
-# controlling the nuclear dynamics (in this case sampling the path Hamiltonian using
-# molecular dynamics) while the calculation of energies and forces is delegated to
-# an external client program, in this example ``LAMMPS``.
-#
-# An i-PI calculation is specified by an XML file.
+# This follows the same steps as the ``path-integrals`` example. One important
+# difference is that we will request the ``scaledcoords`` output, which
+# contains the estimators for the total energy and heat capacity as defined
+# in this `paper <https://arxiv.org/abs/physics/0505109>`_. In order to do this,
+# the ``scaledcoords`` output is added to the relevant section of the ``i-PI``
+# input XML file.
 
 # Open and read the XML file
 with open("data/input.xml", "r") as file:
@@ -72,66 +48,30 @@ with open("data/input.xml", "r") as file:
 print(xml_content)
 
 # %%
-# NB1: In a realistic simulation you may want to increase the field
+# NB1: In a realistic simulation you will want to increase the field
 # ``total_steps``, to simulate at least a few 100s of picoseconds.
 #
 # NB2: To converge a simulation of water at room temperature, you
-# typically need at least 32 beads. We will see later how to accelerate
-# convergence using a colored-noise thermostat, but you can try to
-# modify the input to check convergence with conventional PIMD
+# typically need at least 32 beads.
 
 # %%
-# i-PI and lammps should be run separately, and it is possible to
-# launch separate lammps processes to parallelize the evaluation over
-# the beads. On the the command line, this amounts to launching
-#
-# .. code-block:: bash
-#
-#    i-pi data/input.xml > log &
-#    sleep 2
-#    lmp -in data/in.lmp &
-#    lmp -in data/in.lmp &
-#
-# Note how ``i-PI`` and ``LAMMPS`` are completely independent, and
-# therefore need a separate set of input files. The client-side communication
-# in ``LAMMPS`` is described in the ``fix_ipi`` section, that matches the socket
-# name and mode defined in the ``ffsocket`` field in the ``i-PI`` file.
-#
-# We can launch the external processes from a Python script as follows
+# We launch the i-PI and LAMMPS processes, exactly as in the
+# ``path-integrals`` example.
 
 ipi_process = subprocess.Popen(["i-pi", "data/input.xml"])
 time.sleep(2)  # wait for i-PI to start
 lmp_process = [subprocess.Popen(["lmp", "-in", "data/in.lmp"]) for i in range(2)]
 
-# %%
-# If you run this in a notebook, you can go ahead and start loading
-# output files *before* i-PI and lammps have finished running, by
-# skipping this cell
-
 ipi_process.wait()
 lmp_process[0].wait()
 lmp_process[1].wait()
 
-
-# %%
-# Analyzing the simulation
-# ~~~~~~~~~~~~~~~~~~~~~~~~
-#
-# After the simulation has run, you can visualize and post-process the trajectory data.
-# Note that i-PI prints a separate trajectory for each bead, as structural properties
-# can be computed averaging over the configurations of any of the beads.
-
-# drops first frame where all atoms overlap
 output_data, output_desc = ipi.read_output("simulation.out")
-traj_data = [ipi.read_trajectory(f"simulation.pos_{i}.xyz")[1:] for i in range(8)]
 
 
 # %%
-# The simulation parameters are pushed at the limits: with the aggressive stochastic
-# thermostatting and the high-frequency normal modes of the ring polymer, there are
-# fairly large fluctuations of the conserved quantity. This is usually not affecting
-# physical observables, but if you see this level of drift in a production run, check
-# carefully for convergence and stability with a reduced time step.
+# Let's plot the potential and conserved energy as a function of time,
+# just to check that the simulation ran sensibly.
 
 fix, ax = plt.subplots(1, 1, figsize=(4, 3), constrained_layout=True)
 ax.plot(
@@ -151,7 +91,9 @@ ax.set_ylabel(r"energy / eV")
 ax.legend()
 
 # %%
-# Comment
+# We will now plot the values of the energy and heat capacity estimators
+# as a function of time.
+# First the energy:
 
 scaledcoords_energy = np.loadtxt("simulation.out")[:, 6]
 
@@ -167,7 +109,7 @@ ax.set_ylabel(r"$E / a.u.$")
 ax.legend()
 
 # %%
-# Comment
+# And, finally, the heat capacity:
 
 scaledcoords_Cv = np.loadtxt("simulation.out")[:, 7]
 
