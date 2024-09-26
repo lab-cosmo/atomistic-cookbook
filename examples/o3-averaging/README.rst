0efa65dff20599051e5d890f3e87c7e704b77f4e
Michele Ceriotti <michele.ceriotti@epfl.ch>
2024-09-26 16:17:58 +0200

    Stub of an o3-averaging example

---
+++ examples/o3-averaging/README.rst
@@ -0,0 +1,6 @@
+Rotational averaging of non-equivariant models
+==============================================
+
+This example shows how to assess (and to an extent correct) the 
+influence of non-equivariant terms in the potential energy computed
+by an unconstrained machine-learning model.
---
+++ examples/o3-averaging/o3-averaging.py
@@ -0,0 +1,90 @@
+r"""
+Rotational averaging of non-equivariant models
+==========================
+
+:Authors: 
+Marcel Langer `@sirmarcel <https://github.com/sirmarcel/>`_; 
+Michele Ceriotti `@ceriottm <https://github.com/ceriottm/>`_ 
+
+DESCRIPTION
+"""
+
+# %%
+
+import bz2
+import os
+import subprocess
+import time
+import xml.etree.ElementTree as ET
+
+import ase
+import ase.io
+import chemiscope
+import ipi
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# %%
+# Show the problem
+# ----------------------------------
+
+
+# %%
+#
+
+# Open and show the relevant part of the input
+xmlroot = ET.parse("data/input-noo3.xml").getroot()
+print("      " + ET.tostring(xmlroot.find(".//ffrotations"), encoding="unicode"))
+
+
+# %%
+# Installing the Python driver
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# i-PI comes with a FORTRAN driver, which however has to be installed
+# from source. We use a utility function to compile it. Note that this requires
+# a functioning build system with `gfortran` and `make`.
+
+ipi.install_driver()
+
+# %%
+# We launch the i-PI and LAMMPS processes, exactly as in the
+# ``path-integrals`` example.
+
+# don't rerun if the outputs already exist
+ipi_process = None
+if not os.path.exists("water-noo3.out"):
+    ipi_process = subprocess.Popen(["i-pi", "data/input-noo3.xml"])
+    time.sleep(2)  # wait for i-PI to start
+    lmp_process = [subprocess.Popen(["lmp", "-in", "data/in.lmp"]) for i in range(1)]
+    driver_process = [
+        subprocess.Popen(
+            ["i-pi-driver", "-u", "-a", "h2o-noo3", "-m", "noo3-h2o"], cwd="data/"
+        )
+        for i in range(1)
+    ]
+
+# %%
+# Skip this cell if you want to run in the background
+if ipi_process is not None:
+    ipi_process.wait()
+    lmp_process[0].wait()
+    driver_process[0].wait()
+
+
+# %%
+#
+# Discuss how molecules get oriented
+
+traj_data = ipi.read_trajectory(
+    "water-noo3.pos_0.extxyz", format="ase", dimension="length"
+)
+
+# %%
+# then, assemble a visualization
+chemiscope.show(
+    frames=traj_data,
+    settings=chemiscope.quick_settings(trajectory=True),
+    mode="structure",
+)
