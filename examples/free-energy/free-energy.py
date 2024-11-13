79be02408fa873512f3af5c25a46753043344e33
DavideTisi <davide.tisi93@gmail.com>
2024-11-13 17:26:21 +0100

    up to harmonic

--- examples/free-energy/free-energy.py
+++ examples/free-energy/free-energy.py
@@ -17,6 +17,9 @@ import os
 import subprocess
 import time
 
+import ipi
+import ase.io
+import chemiscope
 import ipi.utils.parsing as pimdmooc
 import numpy as np
 import matplotlib.pyplot as plt
@@ -48,3 +51,321 @@ import matplotlib.pyplot as plt
 #     B &= 0.000598024968321866~\text{a.u.}\\
 #     \end{align}
 #
+# Let's define a function to visualize the PES!
+#
+
+def PES(x, y , z):
+    """
+    PES from Ref. `Y. Litman et al., JCP (2022)`
+    """
+    
+    A = -0.00476705894242374
+    B = 0.000598024968321866 
+    k = 1837.107 * 0.017314074**2
+    
+    return A * x**2 + B * x**4 + k * y**2 / 2 +  k * z**2 / 2
+
+# %%
+# The PES, in three dimensions, can be drawn with xy contours
+# (each contour corresponds to kB T with T = 300 K), for various z values. 
+#
+
+x = np.linspace(-3,3,21)
+y = np.linspace(-0.5,0.5,21)
+
+X, Y = np.meshgrid(x, y)
+Z = PES(X, Y, 0)
+
+
+contour_levels = [np.min(Z) + 0.00095004347 * i for i in range(12)]
+
+plt.title(r'$V(x,y,0)$')
+plt.xlabel(r'$x$ [a.u.]')
+plt.ylabel(r'$y$ [a.u.]')
+plt.contour(X, Y, Z, levels=contour_levels)
+plt.show()
+
+# %%
+# **Questions**:
+#
+# 1. Looking at the contours can you tell
+# if delocalization of the system to its other minimum is
+# a rare event with respect to the time scale of
+# the system's vibrations?
+# 
+# 2. Feel free to plot it for various z values.
+# Does the plot change? What does it tell you
+# about the coupling between the modes?
+#
+#
+
+# %%
+# Calculating the free energy of a hamiltonian
+# ============================================
+#
+# The harmonic approximation
+# --------------------------
+#
+# The harmonic approximation to the PES is essentially
+# a truncated Taylor series expansion to second
+# order around one of its minima.
+# :math:`V^{\text{harm}} = V(q_0) + \frac{2}{2} \left.\frac{\partial^2 V}{\partial q^2}\right|_{q_0} (q - q_0)^2`  
+# where :math:`q = (x,y,z)`
+# is a position vector and :math:`q_0 = \text{arg min}_{q} V` is
+# the position where the PES has a local minimum.
+#
+# Let's use the i-PI code to optimize the PES with respect to its position
+# to find :math:`q_0` and :math:`V^{\text{harm}}`.
+#
+
+# %%
+# Step 1: fixed cell geometry optimization
+# ----------------------------------------
+# 
+# To find :math:`q_0` we will use the fixed-cell geometry
+# optimization feature of i-PI! The inputs are
+# into the `geop` directory.
+#
+# You will find the i-PI input file. Have a look at the
+# `input.xml` file and see if you can understand its various parts.
+#
+# The snippet that implements the geometry optimization feature is 
+#
+# .. code-block:: xml
+#
+#    <motion mode='minimize'>
+#      <optimizer mode='bfgs'>
+#        <tolerances>
+#          <energy> 1e-5 </energy>
+#          <force> 1e-5 </force>
+#          <position> 1e-5 </position>
+#        </tolerances>
+#      </optimizer>
+#    </motion>
+#
+#
+# In essence, the geometry optimization is implemented as a
+# motion class. At every step instead of performing "dynamics" we will
+# simply move towards a local minimum of the PES. There are
+# many algorithms for locally optimizing high-dimensional functions;
+# here we use the robust
+# `BFGS <https://en.wikipedia.org/wiki/Broyden%E2%80%93Fletcher%E2%80%93Goldfarb%E2%80%93Shanno_algorithm>`_
+# method. The tolerances set thershold values for changes in the energy,
+# positions and forces, that are sufficient to deem an optimization converged. 
+#
+
+# %%
+# Installing the Python driver
+#
+# i-PI comes with a FORTRAN driver, which however has to be installed
+# from source. We use a utility function to compile it. Note that this requires
+# a functioning build system with `gfortran` and `make`.
+
+ipi.install_driver()
+# %%
+# To perform a fixed-cell geomerty optimization
+# the bash script command should look like: 
+# 
+# .. code-block:: bash
+#
+#      i-pi geop/input.xml > log.i-pi $
+#      sleep(5)
+#      i-pi-driver -u -h geop -m doublewell_1D & 
+# 
+# The same can be achieved from Python using `subprocess.Popen`
+
+
+ipi_process = subprocess.Popen(["i-pi", "geop/input.xml"])
+time.sleep(5)  # wait for i-PI to start
+driver_process =subprocess.Popen(
+            ["i-pi-driver", "-u", "-h", "geop", "-m", "doublewell_1D"]
+        )
+    
+# %%
+# If you run this in a notebook, you can go ahead and start loading
+# output files *before* i-PI and lammps have finished running, by
+# skipping this cell
+
+ipi_process.wait()
+driver_process.wait()
+    
+# %%
+#
+# The number of steps taken for an optimization calculation depends on
+# the system (the PES), the optimization method,
+# and the initial configuration. Here,
+# we initialize the system from :math:`q = (2.0, 0.2, -0.5)`
+# as seen in the `geop/init.xyz` file.
+#
+# You can analyze your optimization calculation by plotting
+# the potential energy vs steps and confirm if the potential
+# energy has indeed converged to the set threshold!
+# 
+# The final frame of the `geop.pos_0.xyz` file gives 
+# with chemiscope we can wisualize the minimzation 
+# trajectory and its energies.
+#
+
+geop_out,_geop = ipi.read_output('geop.out')
+geop_traj = ipi.read_trajectory("geop.pos_0.xyz")
+
+chemiscope.show(
+    frames=geop_traj,
+    properties={
+        "steps": { "values" : np.arange(len(geop_traj)),
+                  "target" : "structure"},
+        "U":{"values": geop_out['potential'],
+              "target" : "structure"},
+    }, 
+    settings=chemiscope.quick_settings(
+        x="steps", y="U", trajectory=True
+    ),
+    mode="structure",
+)
+
+# %%
+# Step 2: harmonic calculation
+# ----------------------------
+#
+# To compute the system's Hessian we will use the vibrations feature of i-PI. 
+# Go through the `input.xml` file.
+# The snippet that implements the vibrations feature is 
+# 
+# .. code-block:: xml
+#
+#    <motion mode='vibrations'>
+#      <vibrations mode='fd'>
+#        <pos_shift> 0.001 </pos_shift>
+#        <energy_shift> 0.001 </energy_shift>
+#        <prefix> phonons </prefix>
+#        <asr> none </asr>
+#      </vibrations>
+#    </motion>
+#
+# The system's Hessian is computed using the finite difference method.
+# This approach approximates the ij-th element of the Hessian in terms
+# of the forces acting at infinitisemally displaced positions around the minimum:
+#
+# .. math::
+#
+#      \frac{\partial^2 V}{\partial q^2}_{ij} \approx - \frac{1}{2 \epsilon}
+#      \left(\left.f_{i}\right|_{q_{j} + \epsilon} - \left.f_{i}\right|_{q_{j}
+#      - \epsilon} \right)
+#
+# At every step instead of performing "dynamics", we will displace a degree of
+# freedom along :math:`\pm \epsilon` and estimate one row of the Hessian. `pos_shift` sets
+# the value of :math:`\epsilon` while `asr` zeros out the blocks of the Hessian due to continuous
+# symmetries (translations or rotations for solids or clusters). In this example,
+# we set this option to `none` as our system doesn't possess any continuous symmetries.
+#
+
+# %%
+# The initial configuration for this calculation should correspond to an
+# opitimized position. You can obtain this from the last frame of the geop trajectory. 
+
+ase.io.write('init_harm.xyz',geop_traj,format='extxyz')
+
+# %%
+# The `bash` command for this would be
+# 
+# .. code-block:: bash
+#
+#      i-pi harm/input.xml > log.i-pi $
+#      sleep(5)
+#      i-pi-driver -u -h vib -m doublewell_1D & 
+# 
+# Again we will use `subprocess.Popen`
+
+ipi_process = subprocess.Popen(["i-pi", "harm/input.xml"])
+time.sleep(5)  # wait for i-PI to start
+driver_process =subprocess.Popen(
+            ["i-pi-driver", "-u", "-h", "vib", "-m", "doublewell_1D"]
+        )
+
+# %%
+# If you run this in a notebook, you can go ahead and start loading
+# output files *before* i-PI and lammps have finished running, by
+# skipping this cell
+
+ipi_process.wait()
+driver_process.wait()
+
+# %%
+# The Hessian can be recovered from the `harm.phonons.hess`file. 
+# You can use the snippet below to plot the harmonic approximation
+# to the PES
+#
+
+def PES_harm(x,y,z):
+    """
+    Harmonic approximation to the PES from Ref. `Y. Litman et al., JCP (2022)` around 
+    a local minimum. Note this function is only valid for the example!
+    """
+    
+    return V0 + hess[0,0] * (x - q0[0])**2 / 2 + hess[1,1] * (y - q0[1])**2 / 2 + hess[2,2] * (z - q0[2])**2 / 2
+
+hess = np.loadtxt('harm.phonons.hess', comments='#')
+q0 = ipi.read_trajectory('init_harm.xyz')[0].positions[0] / 0.529177
+V0 = np.loadtxt('harm.out', ndmin=2)[0,1]
+
+x = np.linspace(-3,3,21)
+y = np.linspace(-0.5,0.5,21)
+
+X, Y = np.meshgrid(x, y)
+
+contour_levels = [np.min(Z) + 0.00095004347 * i for i in range(12)]
+
+plt.title(r'$V(x,y,0)$')
+plt.xlabel(r'$x$ [a.u.]')
+plt.ylabel(r'$y$ [a.u.]')
+plt.contour(X, Y, PES(X, Y, 0), levels=contour_levels)
+plt.show()
+
+plt.title(r'$V^{\mathrm{harm}}(x,y,0)$')
+plt.xlabel(r'$x$ [a.u.]')
+plt.ylabel(r'$y$ [a.u.]')
+plt.contour(X, Y, PES_harm(X, Y, 0), levels=contour_levels)
+plt.show()
+
+# %%
+# The harmonic calculation also  gives us the frequency modes
+# from the dynamical matrix (mass scaled hessian)!
+#
+
+W2s = np.loadtxt('harm.phonons.eigval')
+print ('Harmonic Frequencies')
+for w2 in W2s:
+    print('%10.5f cm^-1'% (w2**0.5 * 219474))
+
+# %% 
+# These frequencies can be used to calculate the quantum harmonic
+# free energy of the system -- an approxiamtion to its true free energy!
+#
+
+def quantum_harmonic_free_energy(Ws, T):
+    """
+    Receives a list of frequencies in atomic units and the temperature 
+    in Kelvin. Returns the system's harmonic free energy.
+    """
+    
+    hbar = 1
+    kB = 3.1668116e-06
+    
+    return V0 + -kB * T * np.log(1 - np.exp(-hbar * Ws / (kB * T))).sum() 
+
+print ('Quantum Harmonic free energy: %15.8f [a.u.]'% (quantum_harmonic_free_energy(W2s**0.5, 300)))
+print ('Quantum Harmonic free energy: %15.8f [eV]'% (quantum_harmonic_free_energy(W2s**0.5, 300) * 27.211386))
+print ('Quantum Harmonic  free energy: %15.8f [kJ/mol]' % (quantum_harmonic_free_energy(W2s**0.5, 300) * 2625.4996))
+
+# %%
+#
+# The exact free energy for the system when localized in one of
+# the wells is still around 0.65 kJ/mol off w.r.t the harmonic limit. 
+#
+
+F = -7.53132797e-05 + quantum_harmonic_free_energy(W2s**0.5, 300)
+
+print ('Exact Quantum free energy: %15.8f [a.u.]'% (F))
+print ('Exact Quantum free energy: %15.8f [eV]'% (F * 27.211386))
+print ('Exact Quantum free energy: %15.8f [kJ/mol]' % (F * 2625.4996))
+
