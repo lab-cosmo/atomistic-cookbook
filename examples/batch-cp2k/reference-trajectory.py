28750fffb9577d036bb6d5d6624383b50191cd7a
DavideTisi <davide.tisi93@gmail.com>
2025-02-17 14:26:11 +0100

    test ln

--- examples/batch-cp2k/reference-trajectory.py
+++ examples/batch-cp2k/reference-trajectory.py
@@ -62,7 +62,7 @@ subprocess.run(["echo", "5"], check=True)
 subprocess.run(["ls", "../../.nox/batch-cp2k/"], check=True)
 subprocess.run(["echo", "6"], check=True)
 subprocess.run(["ls", "../../.nox/batch-cp2k/lib"], check=True)
-subprocess.run(["ln", "-s", "../../.nox/batch-cp2k/lib/libsymspg.so.2.5.0", "../../.nox/batch-cp2k/lib/libsymspg.so.1"], check=True)
+subprocess.run(["ln", "-s", "libsymspg.so.2.5.0", "../../.nox/batch-cp2k/lib/libsymspg.so.1"], check=True)
 subprocess.run(["echo", "7"], check=True)
 subprocess.run(["ls", "-l","../../.nox/batch-cp2k/lib/"], check=False)
 
