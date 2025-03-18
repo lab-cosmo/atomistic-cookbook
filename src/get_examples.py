4b5c3c0134ff086c2f5fad15ae61fa4058add39d
Michele Ceriotti <michele.ceriotti@gmail.com>
2025-03-18 09:37:41 +0100

    Fixed bug in example identification logic

--- src/get_examples.py
+++ src/get_examples.py
@@ -12,7 +12,9 @@ EXAMPLES = os.path.join(ROOT, "examples")
 def get_examples():
     """The current list of examples, determined from the directories on disk"""
     return [
-        os.path.basename(os.path.normpath(file)) for file in glob.glob(f"{EXAMPLES}/*")
+        os.path.basename(os.path.normpath(file))
+        for file in glob.glob(f"{EXAMPLES}/*")
+        if os.path.isdir(file)
     ]
 
 
