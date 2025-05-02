9d3ad82b19afceb13028ae2243692216b987e012
Michele Ceriotti <michele.ceriotti@gmail.com>
2025-05-02 12:28:27 +0200

    Fixed bug

--- examples/pet-mad-nc/pet-mad-nc.py
+++ examples/pet-mad-nc/pet-mad-nc.py
@@ -83,7 +83,7 @@ calculator.model.save("pet-mad-latest.pt")
 # Even though the early ML-based interatomic potentials followed this route,
 # .... blah blah and intro to be filled. Reference to our paper.
 
-structure = ase.io.read("data/al6xxx-o2.xyz")
+structure = ase.io.read("data/bimicl.xyz")
 
 structure.calc = calculator
 energy_c = structure.get_potential_energy()
