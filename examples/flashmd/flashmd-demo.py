4e831651c95f1d1cb641219116bd92c1c102a717
frostedoyster <bigi.f@libero.it>
2025-10-24 23:00:10 +0200

    Two steps

--- examples/flashmd/flashmd-demo.py
+++ examples/flashmd/flashmd-demo.py
@@ -148,7 +148,7 @@ sim.set_motion_step(
         sim, flashmd_model_16, device, rescale_energy=True, random_rotation=True
     )
 )
-sim.run(3)  # only run 3 steps, again, slow on CPU
+sim.run(2)  # only run 2 steps, again, slow on CPU
 
 # %%
 # The cell fluctuates around the equilibrium volume, in a way that
