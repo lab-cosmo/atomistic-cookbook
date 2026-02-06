7dc41a2a0cbde10b00609d0b14cc5b29b1974739
Venkat Kapil <venkat.kapil@gmail.com>
2026-02-06 23:07:46 +0000

    within 88 hopefully

--- examples/pigs/pigs.py
+++ examples/pigs/pigs.py
@@ -4,8 +4,10 @@ Multiple time stepping and ring-polymer contraction
 
 :Authors: Venkat Kapil `@DavideTisi <https://github.com/venkatkapil24>`_
 
-This notebook provides an introduction to the path-integral coarsegraining technique
-introduced in F. Musil, I. Zaporozhets, F. Noe, C. Clementi, and V. Kapil, JCP 157(18), 181102 (2022) _
+This notebook provides an introduction to the path-integral 
+coarse-graining technique
+introduced in F. Musil, I. Zaporozhets, F. Noe, 
+C. Clementi, and V. Kapil, JCP 157(18), 181102 (2022) _
 
 If you need an introduction to path integral simulations,
 or to the use of `i-PI <http://ipi-code.org>`_, which is the
