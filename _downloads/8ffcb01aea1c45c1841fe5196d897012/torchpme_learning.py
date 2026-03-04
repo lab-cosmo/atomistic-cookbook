7b6e6e872dd28b54627f8a1bdecc1f565b8f4084
ceriottm <ceriottm@users.noreply.github.com>
2026-03-04 07:00:32 +0000

    deploy: 1fea4e23bd8ca64f0e90fd00aee21e309083aead

---
+++ _downloads/8ffcb01aea1c45c1841fe5196d897012/torchpme_learning.py
@@ -0,0 +1,328 @@
+"""
+Learning Capabilities with torchpme
+=======================================
+
+:Authors: Egor Rumiantsev `@E-Rum <https://github.com/E-Rum/>`_; Philip Loche
+   `@PicoCentauri <https://github.com/PicoCentauri>`_
+
+This example demonstrates the capabilities of the `torchpme` package, focusing on
+learning target charges and utilizing the :class:`CombinedPotential` class to evaluate
+potentials that combine multiple pairwise interactions with optimizable ``weights``.
+
+The ``weights`` are optimized to reproduce the energy of a system interacting purely
+through Coulomb forces.
+"""
+
+# %%
+import os
+import urllib.request
+from typing import Dict
+
+import ase.io
+import ase.visualize.plot
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torchpme import CombinedPotential, EwaldCalculator, InversePowerLawPotential
+from vesin import NeighborList
+
+
+# %%
+# Select computation device
+device = "cpu"
+
+if torch.cuda.is_available():
+    device = "cuda"
+
+dtype = torch.float32
+
+prefactor = 0.5292  # Unit conversion prefactor.
+
+# %%
+# Download and load the dataset
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+data_dir = "data"
+os.makedirs(data_dir, exist_ok=True)
+dataset_url = "https://archive.materialscloud.org/records/405an-d8183/files/point_charges_Training_set_p1.xyz"  # noqa: E501
+dataset_path = os.path.join(data_dir, "point_charges_Training_set.xyz")
+
+if not os.path.isfile(dataset_path):
+    print(f"Downloading dataset from {dataset_url} ...")
+    urllib.request.urlretrieve(dataset_url, dataset_path)
+    print("Download complete.")
+
+# The dataset consists of atomic configurations with reference energies.
+frames = ase.io.read(dataset_path, ":10")
+
+# %%
+# Define model parameters
+cell = frames[0].get_cell().array
+cell_dimensions = np.linalg.norm(cell, axis=1)
+cutoff = np.min(cell_dimensions) / 2 - 1e-6  # Define the cutoff distance.
+smearing = cutoff / 6.0  # Smearing parameter for interaction potentials.
+lr_wavelength = 0.5 * smearing  # Wavelength for long-range interactions.
+
+params = {"lr_wavelength": lr_wavelength}
+
+# %%
+# Build the neighbor list
+# ~~~~~~~~~~~~~~~~~~~~~~~~
+# The neighbor list is used to identify interacting pairs within the cutoff distance.
+nl = NeighborList(cutoff=cutoff, full_list=False)
+
+l_positions = []
+l_cell = []
+l_neighbor_indices = []
+l_neighbor_distances = []
+l_ref_energy = torch.zeros(len(frames), device=device, dtype=dtype)
+
+for i_atoms, atoms in enumerate(frames):
+    # Compute neighbor indices and distances
+    i, j, d = nl.compute(
+        points=atoms.positions, box=atoms.cell.array, periodic=True, quantities="ijd"
+    )
+    i = torch.from_numpy(i.astype(int))
+    j = torch.from_numpy(j.astype(int))
+
+    # Store atom positions, cell information, neighbor indices, and distances
+    l_positions.append(torch.tensor(atoms.positions, device=device, dtype=dtype))
+    l_cell.append(torch.tensor(atoms.cell.array, device=device, dtype=dtype))
+    l_neighbor_indices.append(torch.vstack([i, j]).to(device=device).T)
+    l_neighbor_distances.append(torch.from_numpy(d).to(device=device, dtype=dtype))
+
+    # Store reference energy
+    l_ref_energy[i_atoms] = torch.tensor(
+        atoms.get_potential_energy(), device=device, dtype=dtype
+    )
+
+
+# %%
+# Function to assign charges to atoms
+def assign_charges(atoms, charge_dict: Dict[str, torch.Tensor]) -> torch.Tensor:
+    """Assign charges to atoms based on their chemical symbols."""
+    chemical_symbols = np.array(atoms.get_chemical_symbols())
+    charges = torch.zeros(len(atoms), dtype=dtype, device=device)
+
+    for chemical_symbol, charge in charge_dict.items():
+        charges[chemical_symbols == chemical_symbol] = charge
+
+    return charges.reshape(-1, 1)
+
+
+# %%
+# Define the energy computation
+def compute_energy(charge_dict: Dict[str, torch.Tensor]) -> torch.Tensor:
+    """Compute the total energy based on assigned charges and potentials."""
+    energy = torch.zeros(len(frames), device=device, dtype=dtype)
+    for i_atoms, atoms in enumerate(frames):
+        charges = assign_charges(atoms, charge_dict)
+
+        potential = calculator(
+            charges=charges,
+            cell=l_cell[i_atoms],
+            positions=l_positions[i_atoms],
+            neighbor_indices=l_neighbor_indices[i_atoms],
+            neighbor_distances=l_neighbor_distances[i_atoms],
+        )
+        energy[i_atoms] = (charges * potential).sum()
+
+    return energy
+
+
+# %%
+# Define the loss function
+def loss(charge_dict: Dict[str, torch.Tensor]) -> torch.Tensor:
+    """Calculate the loss as the mean squared error between computed and reference
+    energies."""
+    energy = compute_energy(charge_dict)
+    mse = torch.sum((energy - l_ref_energy) ** 2)
+
+    return mse.sum()  # Optionally add charge_penalty for strict neutrality enforcement.
+
+
+# %%
+# Fit charge model
+# ~~~~~~~~~~~~~~~~~
+
+# %%
+# Set initial values for the potential
+potential = CombinedPotential(
+    potentials=[
+        InversePowerLawPotential(exponent=1.0, smearing=smearing, prefactor=prefactor)
+    ],
+    smearing=smearing,
+)
+calculator = EwaldCalculator(potential=potential, **params)
+calculator.to(device=device, dtype=dtype)
+
+q_Na = torch.tensor(1e-5).to(device=device, dtype=dtype)
+q_Na.requires_grad = True
+
+q_Cl = -torch.tensor(1e-5 + 0.2).to(device=device, dtype=dtype)
+q_Cl.requires_grad = True
+
+charge_dict = {"Na": q_Na, "Cl": q_Cl}
+
+# %%
+# Learning loop:
+# optimize charges to minimize the loss function
+optimizer = torch.optim.Adam([q_Na, q_Cl], lr=0.1)
+
+q_Na_timeseries = []
+q_Cl_timeseries = []
+loss_timeseries = []
+
+for step in range(1000):
+    optimizer.zero_grad()
+
+    charge_dict = {"Na": q_Na, "Cl": q_Cl}
+
+    loss_value = loss(charge_dict)
+    loss_value.backward()
+    optimizer.step()
+
+    if step % 10 == 0:
+        print(
+            f"Step: {step:>5}, Loss: {loss_value.item():>5.2e}, "
+            + ", ".join([f"q_{k}: {v:>5.2f}" for k, v in charge_dict.items()]),
+            end="\r",
+        )
+
+    loss_timeseries.append(float(loss_value.detach().cpu()))
+    q_Na_timeseries.append(float(q_Na.detach().cpu()))
+    q_Cl_timeseries.append(float(q_Cl.detach().cpu()))
+
+    if loss_value < 1e-10:
+        break
+
+# %%
+# Fit kernel model
+# ~~~~~~~~~~~~~~~~
+# The second phase involves optimizing the weights of the combined potential kernels.
+
+# %%
+# Set initial values for the kernel model
+potential = CombinedPotential(
+    [
+        InversePowerLawPotential(exponent=1.0, smearing=smearing, prefactor=prefactor),
+        InversePowerLawPotential(exponent=2.0, smearing=smearing, prefactor=prefactor),
+    ],
+    smearing=smearing,
+)
+
+calculator = EwaldCalculator(potential=potential, **params)
+calculator.to(device=device, dtype=dtype)
+
+# %%
+# Kernel optimization loop:
+# optimize kernel weights to minimize the loss function
+optimizer = torch.optim.Adam(calculator.parameters(), lr=0.1)
+
+weights_timeseries = []
+loss_timeseries = []
+
+for step in range(1000):
+    optimizer.zero_grad()
+
+    # Fix charges to their ideal values for this phase
+    loss_value = loss({"Na": 1.0, "Cl": -1.0})
+    loss_value.backward()
+    optimizer.step()
+
+    if step % 10 == 0:
+        print(
+            f"Step: {step:>5}, Loss: {loss_value.item():>5.2e} "
+            + ", ".join(
+                [
+                    f"w_{i}: {float(v):>5.2f}"
+                    for i, v in enumerate(
+                        calculator.potential.weights.detach().cpu().tolist()
+                    )
+                ]
+            ),
+            end="\r",
+        )
+
+    loss_timeseries.append(float(loss_value.detach().cpu()))
+    weights_timeseries.append(calculator.potential.weights.detach().cpu().tolist())
+
+    if loss_value < 1e-10:
+        break
+
+# %%
+# Plot results
+# ~~~~~~~~~~~~~~~~~~~
+# Visualize the learning process for charges and kernel weights.
+
+palette = [
+    "#EE7733",  # Orange
+    "#0077BB",  # Blue
+    "#33BBEE",  # Light Blue
+    "#EE3377",  # Pink
+    "#CC3311",  # Red
+    "#009988",  # Teal
+    "#BBBBBB",  # Grey
+    "#000000",  # Black
+]
+
+
+def plot_results(fname=None, show_snapshot=True):
+    """
+    Plot the learning process for charges and kernel weights.
+
+    Args:
+        fname (str): File name to save the plot. If None, the plot is not saved.
+        show_snapshot (bool): Whether to show a snapshot of the atomic configuration.
+    """
+    fig, ax = plt.subplots(
+        2,
+        sharex=True,
+        layout="constrained",
+        dpi=200,
+    )
+
+    if show_snapshot:
+        ax_in = fig.add_axes([0.12, 0.14, 0.27, 0.27])
+        ase.visualize.plot.plot_atoms(atoms, ax=ax_in, radii=0.75)
+        ax_in.set_axis_off()
+
+    # Plot charge learning
+    ax[0].plot(q_Na_timeseries, c=palette[0], label=r"Na")
+    ax[0].plot(np.array(q_Cl_timeseries), c=palette[1], label=r"Cl")
+
+    ax[0].set_ylim(-1.3, 1.3)
+    ax[0].axhline(1, ls="dotted", c=palette[0])
+    ax[0].axhline(-1, ls="dotted", c=palette[1])
+    ax[0].legend()
+    ax[0].set_ylabel(r"Charge / e")
+
+    # Plot kernel weight learning
+    ax[1].axhline(1, c=palette[2], ls="dotted")
+    ax[1].axhline(0, c=palette[3], ls="dotted")
+    weights_timeseries_array = np.array(weights_timeseries)
+    ax[1].plot(weights_timeseries_array[:, 0], label="p=1", c=palette[2])
+    ax[1].plot(weights_timeseries_array[:, 1], label="p=2", c=palette[3])
+
+    ax[1].set_ylim(-0.2, 1.2)
+    ax[1].legend()
+    ax[1].set_ylabel("Kernel weight")
+
+    for a in ax:
+        a.set_xscale("log")
+
+    ax[1].set_xlabel("Learning epoch")
+
+    fig.align_labels()
+
+    if fname is not None:
+        fig.savefig(fname, transparent=True, bbox_inches="tight")
+
+    plt.show()
+
+
+# Call the plot function to visualize results
+plot_results(show_snapshot=True)
+
+# %%
---
+++ _downloads/c30ae838fdaa9326592e1ef5414c0d62/gaas-map.py
@@ -0,0 +1,277 @@
+r"""
+PCA/PCovR Visualization of a training dataset for a potential
+=============================================================
+
+:Authors: Michele Ceriotti `@ceriottm <https://github.com/ceriottm/>`_,
+          Giulio Imbalzano
+
+This example uses ``featomic`` and ``metatensor`` to compute
+structural properties for the structures in a training dataset
+for a ML potential.
+These are then used with simple dimensionality reduction algorithms
+(implemented in ``sklearn`` and ``skmatter``) to obtain a simplified
+description of the dataset, that is then visualized using
+``chemiscope``.
+
+This example uses the dataset from
+`Imbalzano (2021) <http://doi.org/10.1103/PhysRevMaterials.5.063804>`_
+and the principal covariate regression scheme as implemented in
+`Helfrecht (2020) <http://doi.org/10.1088/2632-2153/aba9ef>`_.
+"""
+
+# sphinx_gallery_thumbnail_number = 2
+
+import os
+
+import ase
+import ase.io
+import chemiscope
+import numpy as np
+import requests
+from featomic import AtomicComposition, SoapPowerSpectrum
+from matplotlib import pyplot as plt
+from metatensor import mean_over_samples
+from sklearn.decomposition import PCA
+from sklearn.linear_model import RidgeCV
+from skmatter.decomposition import PCovR
+from skmatter.preprocessing import StandardFlexibleScaler
+from urllib3.util.retry import Retry
+
+
+# %%
+# First, we load the structures, extracting some of the properties for
+# more convenient manipulation. These are
+# :math:`\mathrm{Ga}_x\mathrm{As}_{1-x}` structures used in `Imbalzano &
+# Ceriotti (2021) <http://doi.org/10.1103/PhysRevMaterials.5.063804>`__ to
+# train a ML potential to describe the full composition range.
+#
+
+
+def fetch_dataset(filename, base_url, local_path=""):
+    """Helper function to load data with retries on errors."""
+
+    local_file = local_path + filename
+    if os.path.isfile(local_file):
+        return
+
+    # Retry strategy: wait 1s, 2s, 4s, 8s, 16s on 429/5xx errors
+    retry_strategy = Retry(
+        total=5, backoff_factor=1, status_forcelist=[429, 500, 502, 503, 504]
+    )
+    session = requests.Session()
+    session.mount("https://", requests.adapters.HTTPAdapter(max_retries=retry_strategy))
+
+    # Fetch with automatic retry and error raising
+    response = session.get(base_url + filename)
+    response.raise_for_status()
+
+    with open(local_file, "wb") as file:
+        file.write(response.content)
+
+
+filename = "gaas_training.xyz"
+fetch_dataset(filename, "https://zenodo.org/records/10566825/files/")
+
+structures = ase.io.read(filename, ":")
+energy = np.array([f.info["energy"] for f in structures])
+natoms = np.array([len(f) for f in structures])
+
+
+# %%
+# Remove atomic energy baseline
+# -----------------------------
+#
+# Energies from an electronic structure calculation contain a very large
+# “self” contributions from the atoms, which can obscure the important
+# differences in cohesive energies between structures. We can build an
+# approximate model based on the chemical nature of the atoms, :math:`a_i`
+#
+# .. math::  E(A) = \sum_{i\in A} e_{a_i}
+#
+# where :math:`e_a` are atomic energies that can be determined by linear
+# regression.
+#
+
+# featomic has an `AtomicComposition` calculator that streamlines
+# this (simple) calculation
+calculator = AtomicComposition(**{"per_system": True})
+rho0 = calculator.compute(structures)
+
+# the descriptors are returned as a `TensorMap` object, that contains
+# the composition data in a sparse storage format
+rho0
+
+# for easier manipulation, we extract the features as a dense vector
+# of composition weights
+comp_feats = rho0.keys_to_properties(["center_type"]).block(0).values
+
+# a one-liner to fit a linear model and compute "dressed energies"
+atom_energy = (
+    RidgeCV(alphas=np.geomspace(1e-8, 1e2, 20))
+    .fit(comp_feats, energy)
+    .predict(comp_feats)
+)
+cohesive_peratom = (energy - atom_energy) / natoms
+
+
+# %%
+# The baseline makes up a large fraction of the total energy, but actually
+# the residual (which is the part that matters) is still large.
+#
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+ax.plot(energy / natoms, atom_energy / natoms, "b.")
+ax.set_xlabel("Energy / (eV/atom)")
+ax.set_ylabel("Atomic e. / (eV/atom)")
+plt.show()
+print(f"RMSE / (eV/atom): {np.sqrt(np.mean((cohesive_peratom) ** 2))}")
+
+
+# %%
+# Compute structural descriptors
+# ------------------------------
+#
+# In order to visualize the structures as a low-dimensional map, we start
+# by computing suitable ML descriptors. Here we have used ``featomic`` to
+# evaluate average SOAP features for the structures.
+#
+
+# hypers for evaluating features
+hypers = {
+    "cutoff": {"radius": 4.5, "smoothing": {"type": "ShiftedCosine", "width": 0.5}},
+    "density": {"type": "Gaussian", "width": 0.3},
+    "basis": {
+        "type": "TensorProduct",
+        "max_angular": 4,
+        "radial": {"type": "Gto", "max_radial": 5},
+    },
+}
+calculator = SoapPowerSpectrum(**hypers)
+rho2i = calculator.compute(structures)
+
+# neighbor types go to the keys for sparsity (this way one can
+# compute a heterogeneous dataset without having blocks of zeros)
+rho2i = rho2i.keys_to_samples(["center_type"]).keys_to_properties(
+    ["neighbor_1_type", "neighbor_2_type"]
+)
+
+# computes structure-level descriptors and then extracts
+# the features as a dense array
+rho2i_structure = mean_over_samples(rho2i, sample_names=["atom", "center_type"])
+rho2i = None  # releases memory
+features = rho2i_structure.block(0).values
+
+
+# %%
+# We standardize (per atom) energy and features (computed as a *mean* over
+# atomic environments) so that they can be combined on the same footings.
+
+sf_energy = StandardFlexibleScaler().fit_transform(cohesive_peratom.reshape(-1, 1))
+sf_feats = StandardFlexibleScaler().fit_transform(features)
+
+
+# %%
+# PCA and PCovR projection
+# ------------------------
+#
+# Computes PCA projection to generate low-dimensional descriptors that
+# reflect structural diversity. Any other dimensionality reduction scheme
+# could be used in a similar fashion.
+
+pca = PCA(n_components=4)
+pca_features = pca.fit_transform(sf_feats)
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+scatter = ax.scatter(pca_features[:, 0], pca_features[:, 1], c=cohesive_peratom)
+ax.set_xlabel("PCA[1]")
+ax.set_ylabel("PCA[2]")
+cbar = fig.colorbar(scatter, ax=ax)
+cbar.set_label("energy / eV/at.")
+plt.show()
+
+# %%
+# We also compute the principal covariate regression (PCovR) descriptors,
+# that reduce dimensionality while combining a variance preserving
+# criterion with the requirement that the low-dimensional features are
+# capable of estimating a target quantity (here, the energy).
+#
+
+pcovr = PCovR(n_components=4)
+pcovr_features = pcovr.fit_transform(sf_feats, sf_energy)
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 4))
+scatter = ax.scatter(pcovr_features[:, 0], pcovr_features[:, 1], c=cohesive_peratom)
+ax.set_xlabel("PCovR[1]")
+ax.set_ylabel("PCovR[2]")
+cbar = fig.colorbar(scatter, ax=ax)
+cbar.set_label("energy / (eV/at.)")
+plt.show()
+
+# %%
+# Chemiscope visualization
+# ------------------------
+#
+# Visualizes the structure-property map using a chemiscope widget (and
+# generates a .json file that can be viewed on
+# `chemiscope.org <https://chemiscope.org>`__).
+#
+
+# extracts force data (adding considerably to the dataset size...)
+force_vectors = chemiscope.ase_vectors_to_arrows(structures, key="forces", scale=1)
+force_vectors["parameters"]["global"]["color"] = 0x505050
+
+# adds properties to the ASE frames
+for i, f in enumerate(structures):
+    for j in range(len(pca_features[i])):
+        f.info["pca_" + str(j + 1)] = pca_features[i, j]
+for i, f in enumerate(structures):
+    for j in range(len(pcovr_features[i])):
+        f.info["pcovr_" + str(j + 1)] = pcovr_features[i, j]
+for i, f in enumerate(structures):
+    f.info["cohesive_energy"] = cohesive_peratom[i]
+    f.info["x_ga"] = comp_feats[i, 0] / comp_feats[i].sum()
+
+# it would also be easy to add the properties manually, this is just a dictionary
+structure_properties = chemiscope.extract_properties(structures)
+
+cs = chemiscope.show(
+    structures=structures,
+    properties=structure_properties,
+    shapes={"forces": force_vectors},
+    # the settings are a tad verbose, but give full control over the visualization
+    settings={
+        "map": {
+            "x": {"property": "pcovr_1"},
+            "y": {"property": "pcovr_2"},
+            "color": {"property": "x_ga"},
+        },
+        "structure": [
+            {
+                "bonds": True,
+                "unitCell": True,
+                "shape": ["forces"],
+                "keepOrientation": False,
+            }
+        ],
+    },
+    meta={
+        "name": "GaAs training data",
+        "description": """
+A collection of Ga(x)As(1-x) structures to train a MLIP,
+including force and energy data.
+""",
+        "authors": ["Giulio Imbalzano", "Michele Ceriotti"],
+        "references": [
+            """
+G. Imbalzano and M. Ceriotti, 'Modeling the Ga/As binary system across
+temperatures and compositions from first principles,'
+Phys. Rev. Materials 5(6), 063804 (2021).
+""",
+            "Original dataset: https://archive.materialscloud.org/record/2021.226",
+        ],
+    },
+)
+
+cs.save("gaas_map.chemiscope.json.gz")
+
+cs  # display if in a notebook
